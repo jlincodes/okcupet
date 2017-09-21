d1cef04ab426ca4645c3bd182f23389939f5951b
Julie <julie.lin0812@gmail.com>
2017-09-21 11:54:53 -0700

    Fixes error when logged in

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,8 +7,10 @@ import {
   HashRouter
 } from 'react-router-dom';
 
+import { AuthRoute } from '../util/route_util';
 import NavBarContainer from './nav/nav_bar_container';
-import {AuthRoute } from '../util/route_util';
+import LoginFormContainer from './session_form/login_form_container';
+import SignUpFormContainer from './session_form/sign_up_form_container';
 
 const App = () => (
   <div>
@@ -18,6 +20,8 @@ const App = () => (
       </div>
     </header>
 
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignUpFormContainer} />
 
   </div>
 );
