139025f864656bcc6d4c02e0c12cdb51e70955d7
Julie <julie.lin0812@gmail.com>
2017-09-25 15:58:33 -0700

    Adds conversation reducer to frontend

---
+++ frontend/reducers/conversation_reducer.js
@@ -0,0 +1,23 @@
+import {
+  RECEIVE_ALL_CONVERSATIONS,
+  RECEIVE_CONVERSATION } from '../actions/conversation_actions';
+import merge from 'lodash/merge';
+
+const defaultState = {
+  conversations: {},
+  conversation: {},
+};
+
+const ConversationReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_CONVERSATIONS:
+      return action.conversations;
+    case RECEIVE_CONVERSATION:
+      return merge({}, state, {[action.conversation.id]: action.conversation});
+    default:
+      return state;
+  }
+};
+
+export default ConversationReducer;
