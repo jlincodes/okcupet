a725c35c3e55a6e556141d743618debbbcc00881
Julie <julie.lin0812@gmail.com>
2017-09-25 16:08:11 -0700

    nests messages routes under conversations route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
     resources :users, only: %i(create show)
     resource :session, only: %i(create destroy)
     resources :pets, only: %i(index show create)
-    resources :conversations, only: %i(index show create)
-    resources :messages, only: %i(index show create)
+    resources :conversations, only: %i(index show create) do
+      resources :messages, only: %i(index show create)
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
