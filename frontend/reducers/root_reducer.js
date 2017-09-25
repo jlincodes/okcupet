d51236361ddb014d592fa7b97d94dc961ebd9624
Julie <julie.lin0812@gmail.com>
2017-09-25 16:10:19 -0700

    Add conversation reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,12 +3,14 @@ import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
 import PetReducer from './pet_reducer';
 import UserReducer from './user_reducer';
+import ConversationReducer from './conversation_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   errors: ErrorsReducer,
   pets: PetReducer,
-  user: UserReducer
+  user: UserReducer,
+  conversation: ConversationReducer
 });
 
 export default RootReducer;
