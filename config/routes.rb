77e8223ac28f99271b91d48f8a2ecb4c9b1278d1
Julie <julie.lin0812@gmail.com>
2017-09-25 12:40:57 -0700

    Cleans up files and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: %i(:create :show)
-    resource :session, only: %i(:create :destroy)
+    resources :users, only: %i(create show)
+    resource :session, only: %i(create destroy)
     resources :pets, only: %i(index show create)
     resources :conversations, only: %i(index show create)
     resources :messages, only: %i(index show create)
