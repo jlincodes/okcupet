bcf703a415e9c56760a09b8af66f2075e07d16d8
Julie <julie.lin0812@gmail.com>
2017-09-26 13:45:58 -0700

    Adds currentUser to mapStateToProps for pet show

--- frontend/components/pets/pet_show_container.jsx
+++ frontend/components/pets/pet_show_container.jsx
@@ -3,9 +3,13 @@ import PetShow from './pet_show';
 
 import { fetchPet } from '../../actions/pet_actions';
 
-const mapStateToProps = (state, ownProps) => ({
-  pet: state.pets[ownProps.match.params.petId]
-});
+const mapStateToProps = (state, ownProps) => {
+  // console.log(state);
+  return ({
+    pet: state.pets[ownProps.match.params.petId],
+    currentUser: state.session.currentUser
+  });
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetchPet: (id) => dispatch(fetchPet(id))
