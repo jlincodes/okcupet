e4500ba22623cac3e3c402f387de8d8b81717a56
Julie <julie.lin0812@gmail.com>
2017-09-26 14:16:24 -0700

    adds createConversation action to mapDispatchToProps

--- frontend/components/pets/pet_show_container.jsx
+++ frontend/components/pets/pet_show_container.jsx
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import PetShow from './pet_show';
 
 import { fetchPet } from '../../actions/pet_actions';
+import { createConversation } from '../../actions/conversation_actions';
 
 const mapStateToProps = (state, ownProps) => {
   // console.log(state);
@@ -12,7 +13,9 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  fetchPet: (id) => dispatch(fetchPet(id))
+  fetchPet: (id) => dispatch(fetchPet(id)),
+  createConversation: (conversation) =>
+    dispatch(createConversation(conversation))
 });
 
 export default connect(
