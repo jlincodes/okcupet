05b42acc66e192b27c1a6d44628a0b1837dd583e
Julie <julie.lin0812@gmail.com>
2017-09-26 16:34:16 -0700

    adds Message Reducer to root Reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,13 +4,15 @@ import ErrorsReducer from './errors_reducer';
 import PetReducer from './pet_reducer';
 import UserReducer from './user_reducer';
 import ConversationReducer from './conversation_reducer';
+import MessageReducer from './message_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
   errors: ErrorsReducer,
   pets: PetReducer,
   user: UserReducer,
-  conversation: ConversationReducer
+  conversation: ConversationReducer,
+  message: MessageReducer
 });
 
 export default RootReducer;
