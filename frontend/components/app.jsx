3b0af3ec5223e103e9314f00e329790e35a947c0
Julie <julie.lin0812@gmail.com>
2017-09-26 10:57:50 -0700

    Adds protected route for individual conversation show pages

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,7 +12,10 @@ import SplashPage from './splash_page/splash_page_container';
 import PetIndexContainer from './pets/pet_index_container';
 import PetShowContainer from './pets/pet_show_container';
 import UserShowContainer from './users/user_show_container';
-import ConversationIndexContainer from './conversation/conversation_index_container';
+import ConversationIndexContainer
+  from './conversation/conversation_index_container';
+import ConversationShowContainer
+  from './conversation/conversation_show_container';
 
 const App = () => (
   <div>
@@ -24,7 +27,8 @@ const App = () => (
       <AuthRoute exact path="/signup" component={SplashPage} />
       <ProtectedRoute path='/users/:userId' component={UserShowContainer} />
       <ProtectedRoute path='/pets/:petId' component={PetShowContainer} />
-      // <ProtectedRoute path='/pets' component={PetIndexContainer} />
+      <ProtectedRoute path="/messages/:conversationId" 
+        component={ConversationShowContainer} />
       <ProtectedRoute path="/messages" component={ConversationIndexContainer} />
       <ProtectedRoute path='/index' component={PetIndexContainer} />
       <Route path='/' component={() => <Redirect to='/index' />} />
