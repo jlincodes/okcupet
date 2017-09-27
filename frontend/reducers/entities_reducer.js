178fa014f4fb54d7d4ba0a7f0f9097e084ee2948
Julie <julie.lin0812@gmail.com>
2017-09-27 09:46:56 -0700

    Adds question reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import PetReducer from './pet_reducer';
 import UserReducer from './user_reducer';
 import ConversationReducer from './conversation_reducer';
 import MessageReducer from './message_reducer';
+import QuestionReducer from './question_reducer';
 
 const EntitiesReducer = combineReducers({
   pets: PetReducer,
   user: UserReducer,
   conversation: ConversationReducer,
-  messages: MessageReducer
+  messages: MessageReducer,
+  questions: QuestionReducer
 });
 
 export default EntitiesReducer;
