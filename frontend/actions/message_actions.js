c5a82677c45b54f43b188b565f6abec07a5a8020
Julie <julie.lin0812@gmail.com>
2017-09-26 22:24:39 -0700

    Adds receiveMessage action & fetchMessage thunk action to message actions

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -1,11 +1,22 @@
 import * as MessageApiUtil from '../util/message_api_util';
 
+export const RECEIVE_MESSAGES = "RECEIVE_MESSAGES";
 export const RECEIVE_MESSAGE = "RECEIVE_MESSAGE";
+
+const receiveMessages = (messages) => ({
+  type: RECEIVE_MESSAGES,
+  messages
+});
 
 const receiveMessage = (message) => ({
   type: RECEIVE_MESSAGE,
   message
 });
+
+export const fetchMessages = (conversationId) => dispatch => (
+  MessageApiUtil.fetchMessages(conversationId)
+  .then(resp => dispatch(receiveMessages(resp)))
+);
 
 export const createMessage = (message) => dispatch => (
   MessageApiUtil.createMessage(message)
