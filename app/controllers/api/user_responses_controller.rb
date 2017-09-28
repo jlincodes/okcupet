76e82a4a91d6f2f7ace64363383b176d04cb910a
Julie <julie.lin0812@gmail.com>
2017-09-28 13:36:06 -0700

    Changes user responses controller index to fetch responses by user_id

--- app/controllers/api/user_responses_controller.rb
+++ app/controllers/api/user_responses_controller.rb
@@ -1,9 +1,15 @@
 class Api::UserResponsesController < ApplicationController
   def index
-    @user_responses = UserResponse.all
+    @user_responses = UserResponse.where(user_id: user_response_params[:user_id])
   end
 
   def show
     @user_response = UserResponse.find(params[:id])
+  end
+
+  private
+
+  def user_response_params
+    params.require(:user_response).permit(:body, :question_id, :user_id)
   end
 end
