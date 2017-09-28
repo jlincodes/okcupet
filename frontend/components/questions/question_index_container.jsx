f9dbbc613081e578bb1a31af0ae963744d35ddbe
Julie <julie.lin0812@gmail.com>
2017-09-28 11:57:56 -0700

    Fetches question responses by petId

--- frontend/components/questions/question_index_container.jsx
+++ frontend/components/questions/question_index_container.jsx
@@ -3,18 +3,20 @@ import QuestionIndex from './question_index';
 import { withRouter } from 'react-router-dom';
 
 import { fetchAllQuestions } from '../../actions/question_actions';
-// import { fetchAllResponses } from '../../actions/response_actions';
+import { fetchPetResponses } from '../../actions/pet_resp_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return ({
     questions: Object.values(state.entities.questions),
     pet: state.entities.pets[ownProps.match.params.petId],
-    user: state.entities.user[ownProps.match.params.userId]
+    user: state.entities.user[ownProps.match.params.userId],
+    petResponses: Object.values(state.entities.petResponses)
   });
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fetchAllQuestions: () => dispatch(fetchAllQuestions()),
+  fetchPetResponses: (petId) => dispatch(fetchPetResponses(petId))
 });
 
 export default withRouter(connect(
