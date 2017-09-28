40e52e4cb151538e3316c6c084ceb06a08fffc05
Julie <julie.lin0812@gmail.com>
2017-09-28 13:39:46 -0700

    Adds userResponses to mapStateToProps and fetchUserReponses to mapDispatchToProps

--- frontend/components/questions/question_index_container.jsx
+++ frontend/components/questions/question_index_container.jsx
@@ -4,19 +4,22 @@ import { withRouter } from 'react-router-dom';
 
 import { fetchAllQuestions } from '../../actions/question_actions';
 import { fetchPetResponses } from '../../actions/pet_resp_actions';
+import { fetchUserResponses } from '../../actions/user_resp_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return ({
     questions: Object.values(state.entities.questions),
     pet: state.entities.pets[ownProps.match.params.petId],
     user: state.entities.user[ownProps.match.params.userId],
-    petResponses: Object.values(state.entities.petResponses)
+    petResponses: Object.values(state.entities.petResponses),
+    userResponses: Object.values(state.entities.userResponses)
   });
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fetchAllQuestions: () => dispatch(fetchAllQuestions()),
-  fetchPetResponses: (petId) => dispatch(fetchPetResponses(petId))
+  fetchPetResponses: (petId) => dispatch(fetchPetResponses(petId)),
+  fetchUserResponses: (userId) => dispatch(fetchUserResponses(userId))
 });
 
 export default withRouter(connect(
