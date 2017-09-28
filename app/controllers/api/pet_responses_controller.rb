82d5a60e3f557f4fa1ef6a664b579831450f21d9
Julie <julie.lin0812@gmail.com>
2017-09-28 11:48:06 -0700

    Changes pet response index to render only by pet_id

--- app/controllers/api/pet_responses_controller.rb
+++ app/controllers/api/pet_responses_controller.rb
@@ -1,9 +1,15 @@
 class Api::PetResponsesController < ApplicationController
   def index
-    @pet_responses = PetResponse.all
+    @pet_responses = PetResponse.where(pet_id: pet_response_params[:pet_id])
   end
 
   def show
     @pet_response = PetResponse.find(params[:id])
+  end
+
+  private
+
+  def pet_response_params
+    params.require(:pet_response).permit(:body, :question_id, :pet_id)
   end
 end
