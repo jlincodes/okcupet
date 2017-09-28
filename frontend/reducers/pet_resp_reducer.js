cd194988b54092295710a0dacf3d5ce84d44af18
Julie <julie.lin0812@gmail.com>
2017-09-28 11:51:23 -0700

    Fixes typo/bug

--- frontend/reducers/pet_resp_reducer.js
+++ frontend/reducers/pet_resp_reducer.js
@@ -1,7 +1,6 @@
 import {
-  RECEIVE_ALL_PET_RESPONSES,
-  RECEIVE_PET_RESPONSE
-} from '../actions/pet_resp_actions';
+  RECEIVE_PET_RESPONSES,
+  RECEIVE_PET_RESPONSE } from '../actions/pet_resp_actions';
 import merge from 'lodash/merge';
 
 const defaultState = {};
@@ -9,10 +8,13 @@ const defaultState = {};
 const PetResponseReducer = (state = defaultState, action) => {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_ALL_PET_RESPONSES:
-      return action.pet_responses;
+    case RECEIVE_PET_RESPONSES:
+      return action.petResponses;
     case RECEIVE_PET_RESPONSE:
-      return merge({}, state, {[action.pet_response.id]: action.pet_response});
+      return merge({}, state, {[action.petResponse.id]: action.petResponse});
+      // const newState = Object.assign({}, state);
+      // newState[action.petResponses.id] = action.petResponses;
+      // return newState;
     default:
       return state;
   }
