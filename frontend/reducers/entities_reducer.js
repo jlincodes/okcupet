628d4776b65f61228e49378d2f47c7fda3c8ad18
Julie <julie.lin0812@gmail.com>
2017-09-28 09:30:16 -0700

    Adds pet and user response reducers to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,13 +4,18 @@ import UserReducer from './user_reducer';
 import ConversationReducer from './conversation_reducer';
 import MessageReducer from './message_reducer';
 import QuestionReducer from './question_reducer';
+import PetResponseReducer from './pet_resp_reducer';
+import UserResponseReducer from './user_resp_reducer';
+
 
 const EntitiesReducer = combineReducers({
   pets: PetReducer,
   user: UserReducer,
   conversation: ConversationReducer,
   messages: MessageReducer,
-  questions: QuestionReducer
+  questions: QuestionReducer,
+  petResponses: PetResponseReducer,
+  userResponses: UserResponseReducer
 });
 
 export default EntitiesReducer;
