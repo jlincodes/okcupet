836eed9bcef3f5bb5a763caabecb465cecf7df26
Julie <julie.lin0812@gmail.com>
2017-09-29 12:27:39 -0700

    Gets ready for production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,10 +3,13 @@ import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  middlewares.push(logger);
+}
+
 export const configureStore = (preloadedState = {}) => (
-  createStore(
-    RootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger)
-  )
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
 );
